a35a996610e63f2b62ccc0f34ea2661f03a619ca
Jing Mai <jingmai@pku.edu.cn>
2022-04-17 11:19:18 +0800

    u

--- scripts/Morph_test.py
+++ scripts/Morph_test.py
@@ -32,18 +32,18 @@ if not os.path.isdir(gray_result_dir):
 for gray_path in gray_paths:
     try:
         img = cv2.imread(gray_path, cv2.IMREAD_GRAYSCALE)
-        _, img_open, img_close, img_erose, img_dilate = mo.MyMethod(
+        _, img_erose, img_dilate, img_open, img_close, _, _, _ = mo.MyMethod(
             img, mode='gray')
         file_name = Path(gray_path).stem
         ext_name = os.path.splitext(gray_path)[-1]
-        cv2.imwrite(os.path.join(gray_result_dir,
-                                 file_name + "_open" + ext_name), img_open)
-        cv2.imwrite(os.path.join(gray_result_dir, file_name +
-                                 "_close" + ext_name), img_close)
         cv2.imwrite(os.path.join(gray_result_dir, file_name +
                                  "_erose" + ext_name), img_erose)
         cv2.imwrite(os.path.join(gray_result_dir, file_name +
                                  "_dilate" + ext_name), img_dilate)
+        cv2.imwrite(os.path.join(gray_result_dir,
+                                 file_name + "_open" + ext_name), img_open)
+        cv2.imwrite(os.path.join(gray_result_dir, file_name +
+                                 "_close" + ext_name), img_close)
     except Exception as e:
         print("Image {} failed: {}".format(gray_path, e))
 # %%
